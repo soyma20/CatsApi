e7eef188793703bf778e00020f6173a12cee58cb
soyma20 <slaviksoyma2014@gmail.com>
2023-01-05 11:57:32 +0200

    feat: add side bar

--- src/app/components/side-bar/side-bar.component.ts
+++ src/app/components/side-bar/side-bar.component.ts
@@ -1,4 +1,10 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {select, Store} from "@ngrx/store";
+
+import {ImageService} from "../../services/image.service";
+import {BreedState} from "../../reducers/breed/breed.reducer";
+import {SelectBreed} from "../../reducers/breed/breed.selectors";
+import {BreedSaveAction} from "../../reducers/breed/breed.actions";
 
 @Component({
   selector: 'app-side-bar',
@@ -6,5 +12,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./side-bar.component.css']
 })
 export class SideBarComponent {
+  search: string = '';
+  breed: string[];
 
+  constructor(private imageService: ImageService, private store$: Store<BreedState>) {
+    store$.pipe(select(SelectBreed)).subscribe(value => this.breed = value)
+  }
+
+  catchInputEmit(search: string) {
+    this.search = search
+  }
+
+  findImagesByBreed() {
+    if (this.breed.length !== 0) {
+      this.imageService.getImages().subscribe(value => {
+        this.store$.dispatch(new BreedSaveAction({images: value}))
+      })
+    }
+  }
 }
