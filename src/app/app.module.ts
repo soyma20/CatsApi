650b282ff0ba50379cd5b85fc1c8ba02e3debe7c
soyma20 <slaviksoyma2014@gmail.com>
2023-01-05 12:02:34 +0200

    refactor: clean imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,16 +13,25 @@ import {EffectsModule} from '@ngrx/effects';
 import {StoreRouterConnectingModule} from '@ngrx/router-store';
 import {MatToolbarModule} from "@angular/material/toolbar";
 import {HttpClientModule} from "@angular/common/http";
+import {MatCheckboxModule} from "@angular/material/checkbox";
+import {MatChipsModule} from "@angular/material/chips";
+import {MatIconModule} from "@angular/material/icon";
+import {MatInputModule} from "@angular/material/input";
+import {MatSelectModule} from "@angular/material/select";
 
 import {AppComponent} from './app.component';
 import {AppEffects} from "./app.effects";
 import {metaReducers, reducers} from "./reducers";
-import {SideBarComponent} from './components/side-bar/side-bar.component';
 import {BreedsComponent} from './components/breeds/breeds.component';
+import {SideBarComponent} from './components/side-bar/side-bar.component';
 import {BreedComponent} from './components/breed/breed.component';
 import {SearchInputComponent} from './components/search-input/search-input.component';
 import {ImagesComponent} from './components/images/images.component';
 import {ImageComponent} from './components/image/image.component';
+import {ChipsComponent} from './components/chips/chips.component';
+import {ChipComponent} from './components/chip/chip.component';
+import {PaginatorComponent} from './components/paginator/paginator.component';
+
 
 @NgModule({
   declarations: [
@@ -33,29 +42,37 @@ import {ImageComponent} from './components/image/image.component';
     SearchInputComponent,
     ImagesComponent,
     ImageComponent,
+    ChipsComponent,
+    ChipComponent,
+    PaginatorComponent,
   ],
-  imports: [
-    BrowserModule,
-    HttpClientModule,
-    BrowserAnimationsModule,
-    StoreModule.forRoot(reducers, {
-      metaReducers,
-      runtimeChecks: {
-        strictStateImmutability: true,
-        strictActionImmutability: true
-      }
-    }),
-    StoreDevtoolsModule.instrument({maxAge: 25, logOnly: !isDevMode()}),
-    EffectsModule.forRoot([AppEffects]),
-    StoreRouterConnectingModule.forRoot(),
-    MatToolbarModule,
-    MatSidenavModule,
-    MatFormFieldModule,
-    ReactiveFormsModule,
-    MatButtonModule,
-    MatPaginatorModule,
-    MatCardModule
-  ],
+    imports: [
+        BrowserModule,
+        HttpClientModule,
+        BrowserAnimationsModule,
+        StoreModule.forRoot(reducers, {
+            metaReducers,
+            runtimeChecks: {
+                strictStateImmutability: true,
+                strictActionImmutability: true
+            }
+        }),
+        StoreDevtoolsModule.instrument({maxAge: 25, logOnly: !isDevMode()}),
+        EffectsModule.forRoot([AppEffects]),
+        StoreRouterConnectingModule.forRoot(),
+        MatToolbarModule,
+        MatSidenavModule,
+        MatFormFieldModule,
+        ReactiveFormsModule,
+        MatButtonModule,
+        MatPaginatorModule,
+        MatCardModule,
+        MatCheckboxModule,
+        MatChipsModule,
+        MatIconModule,
+        MatInputModule,
+        MatSelectModule
+    ],
   providers: [],
   bootstrap: [AppComponent]
 })
