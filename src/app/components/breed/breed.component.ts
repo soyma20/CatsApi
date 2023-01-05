dfa154dffe55e500aa7b6d95921ef943e9ba6e2b
soyma20 <slaviksoyma2014@gmail.com>
2023-01-05 12:01:42 +0200

    feat: connect ngrx store

--- src/app/components/breed/breed.component.ts
+++ src/app/components/breed/breed.component.ts
@@ -1,12 +1,37 @@
-import {Component, Input} from '@angular/core';
+import {select, Store} from "@ngrx/store";
+import {Component, DoCheck, Input} from '@angular/core';
+import {Observable} from "rxjs";
+
 import {IBreed} from "../../interfaces/IBreed";
+import {BreedState} from "../../reducers/breed/breed.reducer";
+import {SelectBreed} from "../../reducers/breed/breed.selectors";
+import {BreedAddAction} from "../../reducers/breed/breed.actions";
 
 @Component({
   selector: 'app-breed',
   templateUrl: './breed.component.html',
   styleUrls: ['./breed.component.css']
 })
-export class BreedComponent {
+export class BreedComponent implements DoCheck{
+
+  public breeds$: Observable<string[]> = this.store$.pipe(select(SelectBreed))
+  disabled: boolean = false;
+
   @Input()
-  breed :IBreed;
+  breed: IBreed;
+
+  constructor(private store$: Store<BreedState>) {
+  }
+
+  ngDoCheck(): void {
+    this.breeds$.subscribe(value => {
+      this.disabled =  value.some(value1 => value1 ===this.breed.id);
+    })
+  }
+
+  addBreed() {
+    this.store$.dispatch(new BreedAddAction({breed: this.breed.id}))
+    this.disabled = true
+  }
+
 }
