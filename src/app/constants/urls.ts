08a072e095a22d6e819ba1e8465d1d049a890fb9
soyma20 <slaviksoyma2014@gmail.com>
2023-01-05 11:56:54 +0200

    refactor: change urls

--- src/app/constants/urls.ts
+++ src/app/constants/urls.ts
@@ -3,6 +3,6 @@ import {environment} from '../../environments/environment'
 export const {API,token} = environment;
 
 export const urls = {
-  breeds: API + 'v1/breeds?limit=1000&page=0',
-  images: API + 'v1/images/search?format=json&limit=10&api_key=' + token,
+  breeds: API + 'v1/breeds',
+  images: API + 'v1/images/search?format=json&api_key=' + token,
 }
